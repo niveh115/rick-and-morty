3b273f9c92252db2498df7afd7a4bee9c5cf1b8c
Niv Caspi <nivcaspi3@gmail.com>
2025-06-04 16:12:38 +0300

    added constants

---
+++ scripts/constants.js
@@ -0,0 +1,7 @@
+const BASE_URL = "https://rickandmortyapi.com";
+
+const ELEMENT_ID = {
+  charactersListId: "charactersList",
+  episodesListId: "episodesList",
+  locationsListId: "locationsList",
+};
