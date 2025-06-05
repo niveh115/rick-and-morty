7e08613cf1d7160439392c58127bad923f3a1cd0
kibudi <shahar002000@gmail.com>
2025-06-05 16:11:57 +0300

    updated page

--- scripts/episode-detail.js
+++ scripts/episode-detail.js
@@ -62,19 +62,7 @@ loadEpisodeDetails();
  * @param {Array} characters - Array of character data
  */
 function updateUI(episode, characters) {
-  // TODO: Implement the UI update
-  // 1. Get the detail container element
-  // 2. Create episode header with basic info
-  // 3. Create characters section
-  // 4. For each character:
-  //    - Create a card with image and basic info
-  //    - Make the card link to the character detail page
-  // 5. Handle empty states (no characters)
+
   throw new Error("updateUI not implemented");
 }
 
-// TODO: Initialize the page
-// 1. Get episode ID from URL parameters
-// 2. Validate the ID
-// 3. Load episode details if ID is valid
-// 4. Show error if ID is invalid or missing
